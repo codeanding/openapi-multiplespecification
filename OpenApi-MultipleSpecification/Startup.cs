031dcf17a9a20b1213bf4a670c4851cbff6280b6
Julissa Stefanny Rodriguez Parco <mutatismundi822@gmail.com>
2021-09-24 21:09:58 -0500

    Agregar archivos de proyecto.

---
+++ OpenApi-MultipleSpecification/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenApi_MultipleSpecification
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("ColegioAPIEspEstudiantes", new OpenApiInfo { Title = "Colegio API (Estudiantes)", Version = "v1" });
+                c.SwaggerDoc("ColegioAPIEspDocentes", new OpenApiInfo { Title = "Colegio API (Docentes)", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/ColegioAPIEspEstudiantes/swagger.json", "Colegio API (Estudiantes)");
+                c.SwaggerEndpoint("/swagger/ColegioAPIEspDocentes/swagger.json", "Colegio API (Docentes)");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
